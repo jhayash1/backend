f529003e92f45f38af97a0297619eba43c53c09d
jhayash1 <yash.jha16@gmail.com>
2026-08-06 11:08:29 +0530

    first commit

--- index.js
+++ index.js
@@ -11,75 +11,75 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-// Create a hashed password (normally this comes from your database)
-const hashedPassword = await bcrypt.hash("1234567", 10);
+// // Create a hashed password (normally this comes from your database)
+// const hashedPassword = await bcrypt.hash("1234567", 10);
 
-const user =  {
-    email: "test@gmail.com",
-    password: hashedPassword,
-};
+// const user =  {
+//     email: "test@gmail.com",
+//     password: hashedPassword,
+// };
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
     
-  if (email !== user.email) {
-    return res.status(401).json({
-      message: "Invalid Credentials",
-    });
-  }
+//   if (email !== user.email) {
+//     return res.status(401).json({
+//       message: "Invalid Credentials",
+//     });
+//   }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+//   const isMatch = await bcrypt.compare(password, user.password);
 
-  if (!isMatch) {
-    return res.status(401).json({
-      message: "Invalid Credentials",
-    });
-  }
-  const token = jwt.sign(
-    {
-      email: user.email,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "1h",
-    },
-  );
+//   if (!isMatch) {
+//     return res.status(401).json({
+//       message: "Invalid Credentials",
+//     });
+//   }
+//   const token = jwt.sign(
+//     {
+//       email: user.email,
+//     },
+//     process.env.JWT_SECRET,
+//     {
+//       expiresIn: "1h",
+//     },
+//   );
 
-  return res.status(200).json({
-    message: "Login Successful",
-    token,
-  });
-});
-app.post("/register", async (req,res)=>{
-    const { email, password } = req.body;
+//   return res.status(200).json({
+//     message: "Login Successful",
+//     token,
+//   });
+// });
+// app.post("/register", async (req,res)=>{
+//     const { email, password } = req.body;
 
-  // Check if user already exists
-  const existingUser = user.find((users) => users.email === email);
+//   // Check if user already exists
+//   const existingUser = user.find((users) => users.email === email);
 
-  if (existingUser) {
-    return res.status(400).json({
-      message: "User already exists",
-    });
-  }
-   // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10);
+//   if (existingUser) {
+//     return res.status(400).json({
+//       message: "User already exists",
+//     });
+//   }
+//    // Hash the password
+//   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Save user
-  user.push({
-    email,
-    password: hashedPassword,
-  });
+//   // Save user
+//   user.push({
+//     email,
+//     password: hashedPassword,
+//   });
 
-  return res.status(201).json({
-    message: "Registration Successful",
-  });
-})
-app.get("/profile", auth, (req, res) => {
-   res.json({
-    message: "Profile fetched successfully",
-    user: req.user,
-  });
-});
+//   return res.status(201).json({
+//     message: "Registration Successful",
+//   });
+// })
+// app.get("/profile", auth, (req, res) => {
+//    res.json({
+//     message: "Profile fetched successfully",
+//     user: req.user,
+//   });
+// });
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
